d72c9e876a5796d2e97b4d2f5ec0e1fcf8ebf71f
lakshay <gupta22154006@akgec.ac.in>
2024-12-01 23:50:31 +0530

    Initial Commit

---
+++ Swipe-Task/Swipe_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_TaskApp.swift
+//  Swipe-Task
+//
+//  Created by Lakshay Gupta on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
